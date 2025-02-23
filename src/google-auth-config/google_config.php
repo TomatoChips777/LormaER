ea176c586194f47ce97c7abac8bf6e115c6bc496
Gelobee777 <gelocabase1324@gmail.com>
2025-02-23 19:04:21 +0800

    Resolved merge conflict

--- src/google-auth-config/google_config.php
+++ src/google-auth-config/google_config.php
@@ -4,9 +4,9 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 $client = new Google\Client;
 
-$client->setClientId('');
-$client->setClientSecret("");
-$client->setRedirectUri("http://localhost/LormaER/public/authenticate.php");
+// $client->setClientId('');
+// $client->setClientSecret("");
+// $client->setRedirectUri("http://localhost/LormaER/public/authenticate.php");
 $client->addScope("email");
 $client->addScope("profile");
 $client->setPrompt("select_account");
